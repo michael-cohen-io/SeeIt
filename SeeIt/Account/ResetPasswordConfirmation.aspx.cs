2fde1e80eb1cef0a75f3f42f98394e679238615c
micohen95 <micohen95@gmail.com>
2017-03-15 15:05:38 -0400

    Add project files.

---
+++ SeeIt/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace SeeIt.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ SeeIt/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SeeIt.Startup))]
+namespace SeeIt
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
